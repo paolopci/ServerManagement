2f1f4e32c9be57c0d54bdbdab2cfe813e81be45d
Paolo Paci <paolopci@yahoo.it>
2025-05-09 12:15:18 +0200

    Cascading Parameter crossing render mode boundary -> program.cs

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddCascadingValue("SelectedCity", sp => "Toronto");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
