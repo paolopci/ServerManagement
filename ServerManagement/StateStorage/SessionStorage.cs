18fec0c3d4db0a4876f9bf95db3ac24a38b58963
Paolo Paci <paolopci@yahoo.it>
2025-05-13 17:13:09 +0200

    add SessionStorage Class

---
+++ ServerManagement/StateStorage/SessionStorage.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using ServerManagement.Models;
+
+
+namespace ServerManagement.StateStorage
+{
+    public class SessionStorage
+    {
+        private readonly ProtectedSessionStorage protectedSessionStorage;
+        public SessionStorage(ProtectedSessionStorage protectedSessionStorage)
+        {
+            this.protectedSessionStorage = protectedSessionStorage;
+        }
+
+        public async Task<Server?> GetServerAsync()
+        {
+            var result = await protectedSessionStorage.GetAsync<Server>("server");
+            if (result.Success)
+            {
+                return result.Value;
+            }
+            return null;
+        }
+
+        public async Task SetServerAsync(Server? server)
+        {
+            await protectedSessionStorage.SetAsync("server", server);
+        }
+    }
+}
